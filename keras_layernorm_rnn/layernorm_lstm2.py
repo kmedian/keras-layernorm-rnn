64c1d72a9383042c3250e85f1ba70d5f54342d1b
UH <554c46@gmail.com>
2020-01-15 23:08:06 +0100

    Layernorm LSTM typ2

---
+++ keras_layernorm_rnn/layernorm_lstm2.py
@@ -0,0 +1,269 @@
+import tensorflow as tf
+import tensorflow.keras as keras
+
+
+class LayernormLSTM2Cell(keras.layers.LSTMCell):
+    """LSTM with Layer Normalization (2: fewer large multiplications)
+
+    Notice:
+        LayernormLSTM2Cell(use_layernorm=False) is the same
+        as LSTMCell(implementation=2)
+
+    References:
+    [1] Hochreiter, S., Schmidhuber, J., 1997. Long short-term memory.
+          Neural computation 9, 1735–1780.
+    [2] Ba, Jimmy Lei, Jamie Ryan Kiros, and Geoffrey E. Hinton.
+          "Layer Normalization." ArXiv:1607.06450 [Cs, Stat],
+          July 21, 2016. http://arxiv.org/abs/1607.06450
+
+    """
+    def __init__(
+            self,
+            units,
+            activation='tanh',
+            recurrent_activation='hard_sigmoid',
+            use_bias=True,
+            use_layernorm=True,  # NEW(!)
+            layernorm_epsilon=1e-05,  # NEW(!)
+            kernel_initializer='glorot_uniform',
+            recurrent_initializer='orthogonal',
+            bias_initializer='zeros',
+            gamma_initializer='ones',  # NEW(!)
+            unit_forget_bias=True,
+            kernel_regularizer=None,
+            recurrent_regularizer=None,
+            bias_regularizer=None,
+            gamma_regularizer=None,  # NEW(!)
+            kernel_constraint=None,
+            recurrent_constraint=None,
+            bias_constraint=None,
+            gamma_constraint=None,  # NEW(!)
+            dropout=0.,
+            recurrent_dropout=0.,
+            **kwargs):
+        # store LSTM attributes
+        keras.layers.LSTMCell.__init__(
+            self,
+            units,
+            activation=activation,
+            recurrent_activation=recurrent_activation,
+            use_bias=use_bias,
+            kernel_initializer=kernel_initializer,
+            recurrent_initializer=recurrent_initializer,
+            bias_initializer=bias_initializer,
+            unit_forget_bias=unit_forget_bias,
+            kernel_regularizer=kernel_regularizer,
+            recurrent_regularizer=recurrent_regularizer,
+            bias_regularizer=bias_regularizer,
+            kernel_constraint=kernel_constraint,
+            recurrent_constraint=recurrent_constraint,
+            bias_constraint=bias_constraint,
+            dropout=dropout,
+            recurrent_dropout=recurrent_dropout,
+            implementation=1,
+            dtype=kwargs.get('dtype'),
+            trainable=kwargs.get('trainable', True))
+        # store layernorm attributes
+        self.use_layernorm = use_layernorm
+        self.layernorm_epsilon = layernorm_epsilon
+        self.gamma_initializer = keras.initializers.get(gamma_initializer)
+        self.gamma_regularizer = keras.regularizers.get(gamma_regularizer)
+        self.gamma_constraint = keras.constraints.get(gamma_constraint)
+
+
+    def build(self, input_shape):
+        # build input kernel, recurrent kernel and bias in LSTMCell
+        keras.layers.LSTMCell.build(self, input_shape)
+
+        # build the layernorm objects
+        if self.use_layernorm:
+            ln_config = {
+                'axis': -1,
+                'epsilon': self.layernorm_epsilon,
+                'center': False,
+                'scale': True,
+                'beta_initializer': None,
+                'gamma_initializer': self.gamma_initializer,
+                'beta_regularizer': None,
+                'gamma_regularizer': self.gamma_regularizer,
+                'beta_constraint': None,
+                'gamma_constraint': self.gamma_constraint,
+                'dtype': self.dtype,
+                'trainable': self.trainable
+            }
+            self.layernorm_i = keras.layers.LayerNormalization(**ln_config)
+            self.layernorm_f = keras.layers.LayerNormalization(**ln_config)
+            self.layernorm_c = keras.layers.LayerNormalization(**ln_config)
+            self.layernorm_o = keras.layers.LayerNormalization(**ln_config)
+    
+    def call(self, inputs, states, training=None):
+        # read results from previous time step
+        h_tm1 = states[0]  # previous memory state
+        c_tm1 = states[1]  # previous carry state
+
+        # retrieve dropout mask for input kernel from DropoutRNNCellMixin
+        dp_mask = self.get_dropout_mask_for_cell(inputs, training)
+        
+        # IMPLEMENTATION 2
+        # - One big operations instead of 4 smaller operations
+        # - no droput for recurrent kernel
+
+        # dropout, input kernel
+        if 0. < self.dropout < 1.:
+            inputs = inputs * dp_mask
+        
+        # compute all network connections
+        net = keras.backend.dot(inputs, self.kernel)
+        net += keras.backend.dot(h_tm1, self.recurrent_kernel)
+
+        # split net into the 4 vectors
+        net_i, net_f, net_c, net_o = tf.split(
+            net, num_or_size_splits=4, axis=1)
+
+        # apply scaling of layer normalization to each gate
+        if self.use_layernorm:
+            net_i = self.layernorm_i(net_i)
+            net_f = self.layernorm_f(net_f)
+            net_c = self.layernorm_c(net_c)
+            net_o = self.layernorm_o(net_o)
+
+        # apply bias
+        if self.use_bias:
+            bias_i, bias_f, bias_c, bias_o = tf.split(
+                self.bias, num_or_size_splits=4, axis=0)
+            net_i = keras.backend.bias_add(net_i, bias_i)
+            net_f = keras.backend.bias_add(net_i, bias_f)
+            net_c = keras.backend.bias_add(net_i, bias_c)
+            net_o = keras.backend.bias_add(net_i, bias_o)
+
+        # _compute_carry_and_output_fused
+        i = self.recurrent_activation(net_i)
+        f = self.recurrent_activation(net_f)
+        c = f * c_tm1 + i * self.activation(net_c)
+        o = self.recurrent_activation(net_o)
+
+        # merge all signals into memory state h
+        h = o * self.activation(c)
+
+        # return memory state h, and carry state c
+        return h, [h, c]
+
+
+    def get_config(self):
+        config = {
+            'use_layernorm': self.use_layernorm,
+            'layernorm_epsilon': self.layernorm_epsilon,
+            'gamma_initializer': self.gamma_initializer,
+            'gamma_regularizer': self.gamma_regularizer,
+            'gamma_constraint': self.gamma_constraint
+        }
+
+        cell_config = keras.layers.LSTMCell.get_config(self)
+        del cell_config['name']
+
+        return {**config, **cell_config}
+
+
+
+class LayernormLSTM2(keras.layers.LSTM):
+    def __init__(
+            self,
+            units,
+            activation='tanh',
+            recurrent_activation='hard_sigmoid',
+            use_bias=True,
+            use_layernorm=True,  # NEW(!)
+            layernorm_epsilon=1e-05,  # NEW(!)
+            kernel_initializer='glorot_uniform',
+            recurrent_initializer='orthogonal',
+            bias_initializer='zeros',
+            gamma_initializer='ones',  # NEW(!)
+            unit_forget_bias=True,
+            kernel_regularizer=None,
+            recurrent_regularizer=None,
+            bias_regularizer=None,
+            gamma_regularizer=None,  # NEW(!)
+            activity_regularizer=None,
+            kernel_constraint=None,
+            recurrent_constraint=None,
+            bias_constraint=None,
+            gamma_constraint=None,  # NEW(!)
+            dropout=0.,
+            recurrent_dropout=0.,
+            return_sequences=False,
+            return_state=False,
+            go_backwards=False,
+            stateful=False,
+            unroll=False,
+            **kwargs):
+        # instantiate the RNN cell
+        cell = LayernormLSTM2Cell(
+            units,
+            activation=activation,
+            recurrent_activation=recurrent_activation,
+            use_bias=use_bias,
+            use_layernorm=use_layernorm,  # NEW(!)
+            layernorm_epsilon=layernorm_epsilon,  # NEW(!)
+            kernel_initializer=kernel_initializer,
+            recurrent_initializer=recurrent_initializer,
+            bias_initializer=bias_initializer,
+            gamma_initializer=gamma_initializer,  # NEW(!)
+            unit_forget_bias=unit_forget_bias,
+            kernel_regularizer=kernel_regularizer,
+            recurrent_regularizer=recurrent_regularizer,
+            bias_regularizer=bias_regularizer,
+            gamma_regularizer=gamma_regularizer,  # NEW(!)
+            activity_regularizer=activity_regularizer,
+            kernel_constraint=kernel_constraint,
+            recurrent_constraint=recurrent_constraint,
+            bias_constraint=bias_constraint,
+            gamma_constraint=gamma_constraint,  # NEW(!)
+            dropout=dropout,
+            recurrent_dropout=recurrent_dropout,
+            dtype=kwargs.get('dtype'),
+            trainable=kwargs.get('trainable', True))
+        # call the parent class 'RNN'
+        keras.layers.RNN.__init__(
+            self,
+            cell,
+            return_sequences=return_sequences,
+            return_state=return_state,
+            go_backwards=go_backwards,
+            stateful=stateful,
+            unroll=unroll,
+            **kwargs)
+        # set other parameters
+        self.activity_regularizer = keras.regularizers.get(
+            activity_regularizer)
+        self.input_spec = [keras.layers.InputSpec(ndim=3)]
+        # no cuDNN supoort
+        self.could_use_cudnn = False
+        # return_runtime is a flag for testing (grappler, graph mode)
+        self.return_runtime = kwargs.pop('return_runtime', False)
+
+    @property
+    def use_layernorm(self):
+        return self.cell.use_layernorm
+
+    @property
+    def layernorm_epsilon(self):
+        return self.cell.layernorm_epsilon
+
+    @property
+    def gamma_initializer(self):
+        return self.cell.gamma_initializer
+
+    @property
+    def gamma_regularizer(self):
+        return self.cell.gamma_regularizer
+
+    @property
+    def gamma_constraint(self):
+        return self.cell.gamma_constraint
+
+    def get_config(self):
+        base_config = keras.layers.RNN.get_config(self)
+        del base_config['cell']
+
+        cell_config = self.cell.get_config()
+        return {**base_config, **cell_config}
